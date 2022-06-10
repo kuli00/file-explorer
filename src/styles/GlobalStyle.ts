ce71aae5f4e7853076c5b4d4e15b86ed4660c31e
maciej.kulinski <maciej.kulinski@performance-media.pl>
2022-06-10 19:58:14 +0200

    test

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -11,6 +11,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     overflow: hidden;
     font-family: Helvetica, sans-serif;
+    cursor: url(${cursor}) 24 24, auto;
   }
   
   body {
@@ -18,10 +19,6 @@ const GlobalStyle = createGlobalStyle`
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
-  }
-  
-  * {
-    cursor: url(${cursor}) 24 24, auto;
   }
   
   ::-webkit-scrollbar {
