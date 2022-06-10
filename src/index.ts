d7ed5965816fbe51726692abf59d6fbe34f85777
maciej.kulinski <maciej.kulinski@performance-media.pl>
2022-06-10 19:03:24 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ const createWindow = (): void => {
     frame: false,
   });
 
+  mainWindow.setMenuBarVisibility(false);
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 };
 app.on('ready', createWindow);
